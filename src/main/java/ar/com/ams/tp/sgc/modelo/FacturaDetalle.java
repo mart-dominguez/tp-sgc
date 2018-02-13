633886bd547b46c44cc30c44d48fa86fad8bf334
mart-dominguez <mdomingu@gmail.com>
2018-02-13 14:10:18 -0300

    req02 - mapeo entidades

--- src/main/java/ar/com/ams/tp/sgc/modelo/FacturaDetalle.java
+++ src/main/java/ar/com/ams/tp/sgc/modelo/FacturaDetalle.java
@@ -5,14 +5,71 @@
  */
 package ar.com.ams.tp.sgc.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author martdominguez
  */
+
+@Entity
 public class FacturaDetalle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
     private Integer id;
     private Integer cantidad;
     private Double precioUnitarioVenta;
+    @ManyToOne
+    @JoinColumn(name = "ID_PRODUCTO")
     private Producto producto;
+    @ManyToOne
+    @JoinColumn(name = "ID_FACTURA")
     private Factura factura;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecioUnitarioVenta() {
+        return precioUnitarioVenta;
+    }
+
+    public void setPrecioUnitarioVenta(Double precioUnitarioVenta) {
+        this.precioUnitarioVenta = precioUnitarioVenta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+    
+    
 }
--- src/main/java/ar/com/ams/tp/sgc/modelo/Recibo.java
+++ src/main/java/ar/com/ams/tp/sgc/modelo/Recibo.java
@@ -5,16 +5,72 @@
  */
 package ar.com.ams.tp.sgc.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author martdominguez
  */
+
+@Entity
 public class Recibo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer nroRecibo;
     private Double monto;
+    @ManyToOne
+    @JoinColumn(name="ID_FACTURA")
     private Factura factura;
+    @ManyToOne
+    @JoinColumn(name="ID_CLIENTE")
     private Cliente cliente;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNroRecibo() {
+        return nroRecibo;
+    }
+
+    public void setNroRecibo(Integer nroRecibo) {
+        this.nroRecibo = nroRecibo;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    
     
 }
